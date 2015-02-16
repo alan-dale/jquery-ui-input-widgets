902647fe3cc8bbd0199ceafe42dc8f834facae5a
alan-dale <alandaledev@gmail.com>
2015-02-16 11:10:57 -0500

    Initial commit of the numeric input plugin

---
+++ jquery-ui-input-widgets.js
@@ -0,0 +1,75 @@
+;(function($, window, document, undefined) {
+
+  $.widget("inputWidgets.numericInput", {
+
+    // Defaults
+    options: {
+      min: 0,
+      max: 9999
+    },
+
+    _create: function() {
+      this.options.min = (Number(this.options.min) === this.options.min && this.options.min % 1 === 0) ? this.options.min : 0;
+      this.options.max = (Number(this.options.max) === this.options.max && this.options.max % 1 === 0) ? this.options.max : 9999;
+      this.options.min = (this.options.min > this.options.max) ? this.options.max : this.options.min;
+
+      this._on(this.element, {
+        keypress: function(event) {
+          // If the keypress was non-numeric, don't allow it to be entered
+          if (event.which != 8 && event.which != 0 && (event.which < 48 || event.which > 57)) {
+            return false;
+          }
+        },
+        blur: function(event) {
+          var currentVal = this.element.val();
+          currentVal.replace(/\D/g,'');
+          currentVal = parseInt(currentVal);
+          if (!isNaN(currentVal) && (Number(currentVal) === currentVal && currentVal % 1 === 0)) {
+            if (currentVal < this.options.min) {
+              currentVal = this.options.min;
+            }
+            else if (currentVal > this.options.max) {
+              currentVal = this.options.max;
+            }
+          }
+          else {
+            currentVal = this.options.min;
+          }
+          this.element.val(currentVal);
+
+          this._trigger("blur", event, {
+            value: this.element.val(),
+            min: this.options.min,
+            max: this.options.max
+          });
+        }
+      });
+    },
+
+    _destroy: function() {},
+
+    _setOptions: function(options) {
+      var that = this;
+      $.each(options, function(key, value) {
+        that._setOption(key, value);
+      });
+    },
+
+    _setOption: function(key, value) {
+      switch (key) {
+        case "min":
+          value = (Number(value) === value && value % 1 === 0) ? value : 0;
+          value = (value > this.options.max) ? this.options.max : value;
+          break;
+        case "max":
+          value = (Number(value) === value && value % 1 === 0) ? value : 9999;
+          value = (value < this.options.min) ? this.options.min : value;
+          break;
+        default:
+          break;
+      }
+      this._super(key, value);
+    }
+  });
+
+})(jQuery, window, document);
