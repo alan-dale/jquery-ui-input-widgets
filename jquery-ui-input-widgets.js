0bb08ce728829c40f4a5701878a2566587fb18f9
alan-dale <alandaledev@gmail.com>
2015-02-17 09:26:41 -0500

    Callback changes and logic tweaks
    Removed blur callback and added a refresh function with callback.
    Added functionality to refresh input on Enter keypress. Bug fix to set
    max before min when multiple options are passed to the widget.

--- jquery-ui-input-widgets.js
+++ jquery-ui-input-widgets.js
@@ -1,6 +1,6 @@
 ;(function($, window, document, undefined) {
 
-  $.widget("inputWidgets.numericInput", {
+  $.widget('inputWidgets.numericInput', {
 
     // Defaults
     options: {
@@ -15,34 +15,44 @@
 
       this._on(this.element, {
         keypress: function(event) {
+          // If user hits enter key, refresh the input
+          if (event.which == 13) {
+            this.refresh();
+            return false;
+          }
+
           // If the keypress was non-numeric, don't allow it to be entered
           if (event.which != 8 && event.which != 0 && (event.which < 48 || event.which > 57)) {
             return false;
           }
         },
         blur: function(event) {
-          var currentVal = this.element.val();
-          currentVal.replace(/\D/g,'');
-          currentVal = parseInt(currentVal);
-          if (!isNaN(currentVal) && (Number(currentVal) === currentVal && currentVal % 1 === 0)) {
-            if (currentVal < this.options.min) {
-              currentVal = this.options.min;
-            }
-            else if (currentVal > this.options.max) {
-              currentVal = this.options.max;
-            }
-          }
-          else {
-            currentVal = this.options.min;
-          }
-          this.element.val(currentVal);
-
-          this._trigger("blur", event, {
-            value: this.element.val(),
-            min: this.options.min,
-            max: this.options.max
-          });
+          this.refresh();
         }
+      });
+    },
+
+    refresh: function() {
+      var currentVal = this.element.val();
+      currentVal.replace(/\D/g,'');
+      currentVal = parseInt(currentVal);
+      if (!isNaN(currentVal) && (Number(currentVal) === currentVal && currentVal % 1 === 0)) {
+        if (currentVal < this.options.min) {
+          currentVal = this.options.min;
+        }
+        else if (currentVal > this.options.max) {
+          currentVal = this.options.max;
+        }
+      }
+      else {
+        currentVal = this.options.min;
+      }
+      this.element.val(currentVal);
+
+      this._trigger('refresh', event, {
+        value: this.element.val(),
+        min: this.options.min,
+        max: this.options.max
       });
     },
 
@@ -50,6 +60,14 @@
 
     _setOptions: function(options) {
       var that = this;
+
+      // Need to set max first to avoid minimum logic errors
+      if (typeof options.max != 'undefined') {
+        that._setOption('max', options.max);
+        delete options.max;
+      }
+
+      // Set the other options
       $.each(options, function(key, value) {
         that._setOption(key, value);
       });
@@ -57,11 +75,11 @@
 
     _setOption: function(key, value) {
       switch (key) {
-        case "min":
+        case 'min':
           value = (Number(value) === value && value % 1 === 0) ? value : 0;
           value = (value > this.options.max) ? this.options.max : value;
           break;
-        case "max":
+        case 'max':
           value = (Number(value) === value && value % 1 === 0) ? value : 9999;
           value = (value < this.options.min) ? this.options.min : value;
           break;
